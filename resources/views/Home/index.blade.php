d13345fc443590a1c2bbd6c1bcc263f650cbd272
Qallidi Mohammed AMine <qallidimohammedamine@gmail.com>
2018-02-04 10:39:40 +0000

    Just Commit

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -3,9 +3,49 @@
 @section('title','Home')
 
 @section('content')
+<div class="container">
   <div class="row">
-    <div class="col s12">12-columns (one-half)</div>
-    <div class="col s6">6-columns (one-half)</div>
-    <div class="col s6">6-columns (one-half)</div>
+    <div class="col s12">
+      <br><br>
+      <div class="slider">
+   <ul class="slides">
+     <li>
+       <img src="https://lorempixel.com/580/250/nature/1"> <!-- random image -->
+       <div class="caption center-align">
+         <h3>This is our big Tagline!</h3>
+         <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+       </div>
+     </li>
+     <li>
+       <img src="https://lorempixel.com/580/250/nature/2"> <!-- random image -->
+       <div class="caption left-align">
+         <h3>Left Aligned Caption</h3>
+         <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+       </div>
+     </li>
+     <li>
+       <img src="https://lorempixel.com/580/250/nature/3"> <!-- random image -->
+       <div class="caption right-align">
+         <h3>Right Aligned Caption</h3>
+         <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+       </div>
+     </li>
+     <li>
+       <img src="https://lorempixel.com/580/250/nature/4"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+    </ul>
   </div>
+    </div>
+    <div class="col s6">
+
+    </div>
+    <div class="col s6">
+      
+    </div>
+  </div>
+</div>
 @endsection
--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,4 +1,27 @@
 <footer class="page-footer purple darken-1">
+  <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Founders</h5>
+                <div class="row">
+                  <div class="col l3 s12">
+                    <img src="../../images/founder1.jpg" alt="" class="circle responsive-img">
+                  </div>
+                  <div class="col l3 s12">
+                    <img src="images/yuna.jpg" alt="" class="circle responsive-img">
+                  </div>
+                </div>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Contactez-Nous</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Gmail</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Linkedin</a></li>
+                </ul>
+              </div>
+            </div>
+  </div>
           <div class="footer-copyright">
             <div class="container">
             © 2018 Copyright All Rights Reserved .
